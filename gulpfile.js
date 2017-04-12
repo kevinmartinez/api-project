24e6f23920755703fb3c5d5144817adac489acff
Kevin <kevin@pixelkojan.se>
2017-04-12 12:29:17 +0200

    add browserSync to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp'); // Require Gulp module
 var sass = require('gulp-sass'); // gulp-sass module
+var browserSync = require('browser-sync');
 
 // Gulp task, first argument is the task name to be run with the gulp command, second arg is a function that will run our code
 gulp.task('compileSass', function() {
@@ -11,3 +12,12 @@ gulp.task('compileSass', function() {
         .pipe(gulp.dest('./app/css')); // Where our compiled code will go
 });
 // Run 'gulp compileSass' in terminal
+
+// Run Browsersync with server config
+// TODO: put together a function that compiles sass AND reloads with browser-sync
+gulp.task('browserSync', function(){
+        browserSync({
+        server: 'app',
+        files: ['app/*.html', 'app/css/*.css', 'app/js/*.js']
+    });
+});
