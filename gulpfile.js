38642a0fcada4e7f6c24cc0b3c931df41e5b3c9c
Kevin <kevin@pixelkojan.se>
2017-09-03 17:37:52 +0200

    add concat JS, copy HTML to serve a dist folder for gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,16 @@
 
 var gulp = require('gulp'); // Require Gulp module
 var sass = require('gulp-sass'); // gulp-sass module
-var browserSync = require('browser-sync');
+var browserSync = require('browser-sync');// BrowserSync for autorefreshing in browser
+var concat = require('gulp-concat'); // Require the gulp concat module
 
 // Gulp task, first argument is the task name to be run with the gulp command, second arg is a function that will run our code
 gulp.task('compileSass', function() {
-    // Get .scsss files from all folders in app/scss with a globbing pattern
-    gulp.src('./app/scss/**/*.scss')
+    // Get .scss files from all folders in app/scss with a globbing pattern
+    return gulp.src('./app/scss/**/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./app/css')); // Where our compiled code will go
+        .pipe(gulp.dest('./dist/css'));// Where the compiled code will go
 });
-// Run 'gulp compileSass' in terminal
 
 // Run Browsersync with server config
 // TODO: put together a function that compiles sass AND reloads with browser-sync
@@ -22,9 +22,24 @@ gulp.task('browserSync', function(){
     });
 });
 
+// Concat js files
+gulp.task('concatJS', function() {
+    return gulp.src('./app/js/ajax.js')
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('./dist/js'));
+});
+
+// Serve html files to dist
+gulp.task('serveHTML', function() {
+    return gulp.src('./app/index.html')
+    .pipe(gulp.dest('./dist'));
+});
+
 gulp.task('watch', function() {
     // ** is a so called globbing pattern
     gulp.watch('./app/scss/**/*.scss', ['compileSass']);
+    gulp.watch('./app/js/**/*.js', ['concatJS']);
+    gulp.watch('./app/*.html', ['serveHTML'])
 });
 
 // Our build task
